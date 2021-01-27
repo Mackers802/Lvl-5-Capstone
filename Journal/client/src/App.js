0fc7446c18140da376307455b825d65be5b1b5a9
Mackenzie Yandow <Mackers802@gmail.com>
2021-01-27 10:46:09 -0800

    added build npm

--- Journal/client/src/App.js
+++ Journal/client/src/App.js
@@ -1,6 +1,6 @@
 import React, {useState, useEffect} from "react"
 import axios from "axios"
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import './App.css';
 import InitialForm from "./components/InitialForm.js"
 import JournalEntryCard from "./components/JournalEntryCard.js"
